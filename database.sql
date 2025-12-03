10e2f535e75cd52cae94140be2e797e01abbbf0a
unknown <DominantVortex@proton.me>
2025-12-03 14:06:39 +0100

    add (FK (advisors.advisor_id))

--- database.sql
+++ database.sql
@@ -1,15 +1,15 @@
 CREATE TABLE customers (
   customer_id SERIAL PRIMARY KEY,
-  full_name VARCHAR(50),
-  email VARCHAR(50),
-  phone VARCHAR(10),
+  full_name VARCHAR(50) NOT NULL UNIQUE,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  phone VARCHAR(10) UNIQUE,
   registration_date DATE
 );
 
 CREATE TABLE advisors (
   advisor_id SERIAL PRIMARY KEY,
-  full_name VARCHAR(50),
-  email VARCHAR(50)
+  full_name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE accounts (
@@ -18,10 +18,13 @@ CREATE TABLE accounts (
   balance INT,
   account_type VARCHAR(100),
   customer_id INT,
-  FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE;
+  FOREIGN KEY (advisor_id) REFERENCES advisors(advisor_id) ON DELETE CASCADE;
 );
 
-CREATE TABLE Transactions (
+CREATE TABLE transactions (
   transaction_id SERIAL PRIMARY KEY,
   amount INT,
   transaction_type VARCHAR(100),
